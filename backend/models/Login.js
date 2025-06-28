d94a8f35b07e31dc72d1666e67c412f0f62179bf
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-06-28 23:35:29 +0500

    new commit

--- backend/models/Login.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const loginSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-});
-
-module.exports = mongoose.model('Login', loginSchema); 
-
